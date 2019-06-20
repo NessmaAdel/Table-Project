f66328456fe5f0ee5b23964c88035ad79b3aa8ff
Nesma Adel <nesma.adel@fixedmea.com~>
2019-06-20 18:45:13 +0200

    style Fetch_data

---
+++ orders-table/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Orders from './Components/Orders/orders';
+
+function App() {
+  return (
+    <div className="App">
+      <Orders />
+    </div>
+  );
+}
+
+export default App;
